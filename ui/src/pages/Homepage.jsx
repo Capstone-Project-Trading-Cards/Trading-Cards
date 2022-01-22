74d6ccba8fe424cbcd82ea1bb08f62362c6eb60e
Fatih Camgoz <fatihcamgoz@hotmail.com.tr>
2022-01-21 22:53:07 -0500

    added images and carousel
    added images and carousel

--- ui/src/pages/Homepage.jsx
+++ ui/src/pages/Homepage.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import CardSoccer from "../components/CardSoccer/CardSoccer";
-import Navbar from "../components/CardSoccer/Navbar";
+import Navbar from "../components/Navbar";
+import Carousel from "../components/carousel/Carousel";
 
 export default function Homepage() {
   const [cardData, setCardData] = useState([{}]);
@@ -17,15 +18,50 @@ export default function Homepage() {
       .catch((err) => console.log(err));
   }, []);
   return (
-    <div>
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+      }}
+    >
       <Navbar />
-      <h1 className="text-center">Soccer</h1>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <CardSoccer data={cardData} />
+      <div
+        className="container"
+        style={{
+          minHeight: "94vh",
+          backgroundColor: "white",
+        }}
+      >
+        <br />
+        <br />
+        <div style={{}}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              position: "relative",
+              opacity: 0.9,
+              zIndex: 0,
+            }}
+          >
+            <h1
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50% , -50%)",
+                WebkitTransform: "translate(-50%, -50%)",
+                zIndex: 1,
+                fontFamily: "fantasy",
+                fontSize: 90,
+              }}
+            >
+              Trading Cards Co.
+            </h1>
+            <Carousel />
+          </div>
+        </div>
       </div>
-      <h1 className="text-center">Basketball</h1>
-      <h1 className="text-center">NHL</h1>
-      <h1 className="text-center">NFL</h1>
     </div>
   );
 }
